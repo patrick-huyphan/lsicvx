8ef212c28271d75019934cc69c2e22fb27830323
patrick_huy <pq_huy@bitbuket.org>
2017-07-04 17:41:43 +0700

    add detroy broadcast variables

--- LSI_CVX/src/main/java/pt/spark/sADMM.java
+++ LSI_CVX/src/main/java/pt/spark/sADMM.java
@@ -115,16 +115,16 @@ public class sADMM {
 //        retMat.saveAsTextFile(outFilePath + "/ADMM");
         
         
-        rho0.destroy();
-        lamda.destroy();
-        eps_abs.destroy();
-        eps_rel.destroy();
-        _n.destroy();
-        _m.destroy();
-        _k.destroy();
-        _Bt.destroy();
-        _BtB.destroy();
-        _AtB.destroy();
+        rho0.unpersist(); 
+        lamda.unpersist();
+        eps_abs.unpersist();
+        eps_rel.unpersist();
+        _n.unpersist();
+        _m.unpersist();
+        _k.unpersist();
+        _Bt.unpersist();
+        _BtB.unpersist();
+        _AtB.unpersist();
         
         return retMat.collect();
     }
