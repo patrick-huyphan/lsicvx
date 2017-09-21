2c2c8bc01bc815f129ab78fb174f6ecfbeab38b1
patrick_huy <pq_huy@bitbuket.org>
2017-09-21 19:37:58 +0700

    update LSI_CVX, scc2, ADMM

--- paper/src/main/java/pt/paper/Paper.java
+++ paper/src/main/java/pt/paper/Paper.java
@@ -37,8 +37,8 @@ public class Paper {
 ////        printMat(echelon, false, "echelong");
 //
         double[][] termDocMat = Matrix.Transpose(echelon);          
-        int loop = 18000;
-        clt = new SCCNew2(termDocMat, 0.8, 0.05, 0.01, 1e-5, 1e-5, loop);
+        int loop = 7500;
+        clt = new SCCNew2(termDocMat, 1.05, 0.05, 0.01, 1e-5, 1e-5, loop);
         
 //        clt = new KMeans_Ex4a(termDocMat, 0, 24, new int[]{88, 2, 16, 30,21,24,26,84,34,35,40,58,49,50,54,55,56,67,71,75,80,81,90, 92 });
 
